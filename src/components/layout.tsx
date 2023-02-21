62188fe746fd3823461cc0ed38fd5a0784184a0b
iamBuraks <buraksevinc1949@gmail.com>
2023-02-21 12:59:31 +0300

    head added

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,16 +1,20 @@
 import React, { Suspense } from "react";
 import Navbar from "./navbar/navbar";
+import Head from "next/head";
 
-const Footer = React.lazy(() => import('./footer'))
+const Footer = React.lazy(() => import("./footer"));
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
+      <Head>
+        <title>Burak Sevinç</title>
+      </Head>
       <div className="pt-12 mx-auto min-h-screen max-w-2xl md:px-0 px-8">
         <Navbar />
         <div>{children}</div>
         <Suspense fallback={<div>Loading...</div>}>
-        <Footer />
+          <Footer />
         </Suspense>
       </div>
     </>
