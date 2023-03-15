e2c3bbea9abd4169795c403a4be3bd09e08f3dd1
Burak <burak@192.168.1.65>
2023-03-15 19:40:03 +0300

    project header proile photo fixed

--- src/components/projects/project/header/projectHeader.tsx
+++ src/components/projects/project/header/projectHeader.tsx
@@ -1,5 +1,6 @@
 import { IProject } from "@/types";
 import dayjs from "dayjs";
+import Image from "next/image";
 export default function ProjectHeader({ project }: { project: IProject }) {
   return (
     <>
@@ -12,7 +13,13 @@ export default function ProjectHeader({ project }: { project: IProject }) {
 
       <div className="flex md:flex-row flex-col justify-between md:items-center space-y-2 md:space-y-0">
         <div className="flex items-center space-x-2">
-          <div className="w-8 h-8 bg-slate-200 rounded-full"></div>
+          <div className="w-8 h-8 relative rounded-full overflow-hidden object-cover">
+            <Image
+              src={"/me.png"}
+              alt="Burak Sevinç"
+              fill
+            />
+          </div>
           <p>Burak Sevinç</p>
         </div>
 
